dc1fd53102d2af3c31afca1f6e67ba388163bb5f
stryngs <info@ethicalreporting.org>
2019-02-27 01:12:21 -0500

    Updated mParser for services SQL

---
+++ mParser/lib/list_handler.py
@@ -0,0 +1,41 @@
+import re
+import os
+
+class List(object):
+    """Handle any port list you want to throw at it"""
+    
+    def __init__(self, cn = 'default'):
+        self.tList = []
+        self.sList = []
+        if cn == 'default':
+            self.lType = 'nmap'
+        else:
+            self.lType = None
+        try:
+            os.remove('masscan.sqlite')
+        except:
+            pass
+
+
+    def list_pick(self):
+        if self.lType == 'nmap':
+
+            with open('lists/nmap-services', 'r') as iFile:
+                lst = iFile.read().splitlines()
+            
+            for l in lst:
+                if not re.search('^#', l):
+                    self.tList.append(l)
+
+            count = 1
+            for s in self.tList:
+                count += 1
+                sName = s.split('\t')[0]
+                sPort = s.split('\t')[1].split('/')[0]
+                sProto = s.split('\t')[1].split('/')[1]
+                sFreq = s.split('\t')[2]
+                try:
+                    sCmt = s.split('\t')[3]
+                except:
+                    sCmt = ''
+                self.sList.append((sName, sPort, sProto, sFreq, sCmt))
