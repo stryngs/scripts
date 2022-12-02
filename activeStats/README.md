f9d8343a416f56181f211dd5b0b9e57be09d145e
stryngs <info@ethicalreporting.org>
2022-12-02 03:40:49 -0500

    Moved activeStats to own repo

--- activeStats/README.md
+++
@@ -1,1 +0,0 @@
-# Pull down user and node information from Active Directory, then SQL it for statistics
--- activeStats/get-boxes.ps1
+++
@@ -1,1 +0,0 @@
-﻿Get-ADComputer -Filter 'enabled -eq $true' -Property * | Select-Object Name, Description, DistinguishedName, OperatingSystem | Export-CSV domain-nodes.csv -NoTypeInformation -Encoding UTF8
